cd95b7485dcfb6c711dff32a2b3aea400ca46661
Francisco del Sol <franzuel@hotmail.com>
2023-11-07 23:32:10 +0100

    desplegable

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -11,20 +11,20 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 //local
-//
+/*
 passport.use(new GoogleStrategy({
     clientID: "162867793-krmekskd7524g7fph19coq973942ls8d.apps.googleusercontent.com",
     clientSecret: "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",
     callbackURL: "http://localhost:3000/google/callback"
 },
-/*
+*/
 //desplegable
 passport.use(new GoogleStrategy({
     clientID: "162867793-5a0fuv1dntbo103eaer1car72h9ssrjd.apps.googleusercontent.com",
     clientSecret: "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",
     callbackURL: "https://proyectprocesosfran-dt4p6agska-ew.a.run.app/google/callback"
 },
-*/
+//
     function (accessToken, refreshToken, profile, done) {
         return done(null, profile);
     }
