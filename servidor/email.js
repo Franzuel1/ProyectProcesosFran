1f2e8aebdc78f54d110229db122c46af9d18ca33
Francisco del Sol <franzuel@hotmail.com>
2024-06-08 13:54:36 +0200

    Implementacion de Connect4 (Juego)

--- servidor/email.js
+++ servidor/email.js
@@ -1,42 +1,43 @@
-const nodemailer = require('nodemailer'); // npm install nodemailer
+const nodemailer = require('nodemailer');
 const gv = require('./gestorVariables.js');
 const url = "http://localhost:3000/";
-//const url ="https://proyectprocesosfran-dt4p6agska-ew.a.run.app/";
+// const url = "https://proyectprocesosfran-dt4p6agska-ew.a.run.app/";
 
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'franzuel1@gmail.com',
-        pass: ''
-        //pass: 'rkbf xgxn ehbd dezl' //no es la clave de gmail
+let transporter;
+
+async function configureTransporter() {
+    try {
+        const options = await gv.obtenerOptions();
+        transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: options.user,
+                pass: options.pass
+            }
+        });
+    } catch (error) {
+        console.error('Error configuring transporter:', error);
     }
-});
+}
 
-gv.obtenerOptions(function (res) {
-    options = res;
-    transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: options
-    });
-});
-
-
-//send();
+// Llama a configureTransporter cuando se carga el módulo
+configureTransporter().catch(console.error);
 
 module.exports.enviarEmail = async function (direccion, key, men) {
-    const result = await transporter.sendMail({
-        from: 'franzuel1@gmail.com',
-        to: direccion,
-        subject: 'Confirmar cuenta',
-        text: 'Pulsa aquí para confirmar cuenta',
-        html: '<p>Bienvenido a Sistema</p><p><a href="' + url + 'confirmarUsuario/' + direccion + '/' + key + '">Pulsa aquí para confirmar cuenta</a></p>'
-    });
-    console.log(JSON.stringify(result, null, 4));
-}
-
-module.exports.obtenerOptions = async function (callback) {
-    let options = { user: "franzuel1@gmail.com", pass: "" };
-    let user = await accessCLAVECORREO();
-    options.pass = pass;
-    callback(options);
-}
+    if (!transporter) {
+        console.error("Transporter is not configured yet.");
+        return;
+    }
+    try {
+        const result = await transporter.sendMail({
+            from: 'franzuel1@gmail.com',
+            to: direccion,
+            subject: 'Confirmar cuenta',
+            text: men,
+            html: `<p>Bienvenido a Sistema</p><p><a href="${url}confirmarUsuario/${direccion}/${key}">Pulsa aquí para confirmar cuenta</a></p>`
+        });
+        console.log(JSON.stringify(result, null, 4));
+    } catch (error) {
+        console.error('Error sending email:', error);
+    }
+};
